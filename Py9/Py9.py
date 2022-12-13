525ce7742ad9495a2b1f7b975c3cdfbedc7bd3a6
GreLukas <95912972+GreLukas@users.noreply.github.com>
2022-12-13 11:29:53 +0100

    Add files via upload

--- Py9/Py9.py
+++ Py9/Py9.py
@@ -1,3 +1,4 @@
+#names
 def read_data():
     f = open("names.txt", 'r')
     count = 0
@@ -6,6 +7,17 @@ def read_data():
     for i in x:
         if i == "Darth":
             count +=1
-    print(str(count)+" "+"times")
+    print("Darth - " + str(count)+" "+"times")
 
+    for i in x:
+        if i == "Luke":
+            count +=1
+    print("Luke - " + str(count)+" "+"times")
+
+    for i in x:
+        if i == "Lea":
+            count +=1
+    print("Lea - " + str(count)+" "+"times")
 read_data()
+
+#category
